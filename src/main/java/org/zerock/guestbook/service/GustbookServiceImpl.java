fb27a9a45a49543e25ba7654854be4352ee48374
nakta <nakta0511@gmail.com>
2022-07-24 22:29:24 +0900

    search test

--- src/main/java/org/zerock/guestbook/service/GustbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GustbookServiceImpl.java
@@ -47,7 +47,7 @@ public class GustbookServiceImpl implements GuestbookService{
 
         BooleanBuilder booleanBuilder = getSearch(requestDTO); //검색 조건 처리
 
-        Page<Guestbook> result = repository.findAll(pageable); //Querydsl 사용
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable); //Querydsl 사용
 
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
 
